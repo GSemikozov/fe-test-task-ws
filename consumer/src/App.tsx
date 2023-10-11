4b8a1a6d2cf4c276f6aea3f81e449960b54dc7ad
Herman Semykozov <semikozovgerman@gmail.com>
2023-10-11 17:46:10 +0300

    update logic a bit

--- consumer/src/App.tsx
+++ consumer/src/App.tsx
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { LeaderBoard } from "@/components/leaderboard";
-import { Settings } from "@/components/settings";
-import { useLeaders } from "@/hooks";
-import { Toaster } from "@/components/ui/toaster";
+import React, { useState } from 'react';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { LeaderBoard } from '@/components/leaderboard';
+import { Settings } from '@/components/settings';
+import { useLeaders } from '@/hooks';
+import { Toaster } from '@/components/ui/toaster';
 
 enum AppTabs {
-    leaderboard = 'leaderboard',
-    settings = 'settings'
+  leaderboard = 'leaderboard',
+  settings = 'settings',
 }
 
 export const MIN_LEADERBOARD_DATA_LENGTH = 1;
@@ -15,34 +15,34 @@ export const MAX_LEADERBOARD_DATA_LENGTH = 20;
 export const DEFAULT_LEADERBOARD_DATA_LENGTH = 10;
 
 function App() {
-    const [leaderBoardLength, setLeaderBoardLength] = useState(DEFAULT_LEADERBOARD_DATA_LENGTH);
-    const { data, addLeaderToHiddenList } = useLeaders();
+  const [leaderBoardLength, setLeaderBoardLength] = useState(DEFAULT_LEADERBOARD_DATA_LENGTH);
+  const { data, addLeaderToHiddenList } = useLeaders();
 
-    return (
-        <>
-            <Tabs defaultValue={AppTabs.leaderboard}>
-                <TabsList>
-                    <TabsTrigger value={AppTabs.leaderboard}>{AppTabs.leaderboard}</TabsTrigger>
-                    <TabsTrigger value={AppTabs.settings}>{AppTabs.settings}</TabsTrigger>
-                </TabsList>
-                <TabsContent value={AppTabs.leaderboard}>
-                    <LeaderBoard
-                        data={data.slice(0, leaderBoardLength)}
-                        onRemoveLeader={addLeaderToHiddenList}
-                    />
-                </TabsContent>
-                <TabsContent value={AppTabs.settings}>
-                    <Settings
-                        value={leaderBoardLength}
-                        onChange={setLeaderBoardLength}
-                        minDataLength={MIN_LEADERBOARD_DATA_LENGTH}
-                        maxDataLength={MAX_LEADERBOARD_DATA_LENGTH}
-                    />
-                </TabsContent>
-            </Tabs>
-            <Toaster />
-        </>
-    )
+  return (
+    <>
+      <Tabs defaultValue={AppTabs.leaderboard}>
+        <TabsList>
+          <TabsTrigger value={AppTabs.leaderboard}>{AppTabs.leaderboard}</TabsTrigger>
+          <TabsTrigger value={AppTabs.settings}>{AppTabs.settings}</TabsTrigger>
+        </TabsList>
+        <TabsContent value={AppTabs.leaderboard}>
+          <LeaderBoard
+            data={data.slice(0, leaderBoardLength)}
+            onRemoveLeader={addLeaderToHiddenList}
+          />
+        </TabsContent>
+        <TabsContent value={AppTabs.settings}>
+          <Settings
+            value={leaderBoardLength}
+            onChange={setLeaderBoardLength}
+            minDataLength={MIN_LEADERBOARD_DATA_LENGTH}
+            maxDataLength={MAX_LEADERBOARD_DATA_LENGTH}
+          />
+        </TabsContent>
+      </Tabs>
+      <Toaster />
+    </>
+  );
 }
 
-export default App
+export default App;
